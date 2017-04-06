1917e39ff7cecff4e86dc32f4ebbb409fcc0fc3d
jordandevolder <contact@jordandevolder.fr>
2017-04-06 13:40:40 +0200

    changement dû aux calendrier

--- Vue/planif.php
+++ Vue/planif.php
@@ -12,9 +12,8 @@
 
 <h1>Planification</h1>
 
-<div class="container">
     <div class="row">
-        <div class='col-sm-6'>
+        <div class='col-sm-3'>
             <div class="form-group">
                 <div class='input-group date' id='datetimepicker1'>
                     <input type='text' class="form-control" />
@@ -26,11 +25,12 @@
         </div>
         <script type="text/javascript">
             $(function () {
-                $('#datetimepicker1').datetimepicker();
+                $('#datetimepicker1').datetimepicker({
+
+                });
             });
         </script>
     </div>
-</div>
 
 <?php
 
