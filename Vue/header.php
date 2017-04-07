0162869eba135448c9d2a4b7929810df8848b3ff
Lokmane <Lokmane@air-de-lokmane.home>
2017-04-07 09:53:49 +0200

    Correction des asset

--- Vue/header.php
+++ Vue/header.php
@@ -5,6 +5,9 @@
 ?>
 <head>
     <meta charset="utf-8"/>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="/Vue/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" /><link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
     <link href="Vue/css/bootstrap.css" rel="stylesheet" type="text/css"/>
    <!-- <link href="Vue/css/normalize.css" rel="stylesheet" type="text/css"/> -->
     <link href="Vue/css/style.css" rel="stylesheet" type="text/css"/>
@@ -15,4 +18,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="/Vue/bower_components/moment/min/moment.min.js"></script>
+    <script type="text/javascript" src="/Vue/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/Vue/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
 </head>
